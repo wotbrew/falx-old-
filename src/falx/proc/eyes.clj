fb6b2de51faa57c5cba168632b79232c977724e9
Daniel Stone <dan.stone16321@gmail.com>
2015-02-01 02:19:31 +0000

    minor tidy up

--- src/falx/proc/eyes.clj
+++ src/falx/proc/eyes.clj
@@ -10,30 +10,34 @@
 
 (def eye-tick 200)
 
+(defn just-do-eyes!
+  [game e]
+  (let [map (att game e :map)
+        last-points (att game e :visible-points)
+        last-visible (att game e :visible-entities)
+
+        points (into #{} (find-visible-points game e))
+        entities (into (im/int-set) (find-entities-in game map points))
+
+        now-seen-points (set/difference points last-points)]
+
+        (send state/game set-att e
+              :explored-points (into (att game e :explored-points #{}) now-seen-points)
+              :visible-points points
+              :visible-entities entities)))
+
 (defn should-do-eyes?
-  [e]
-  (on-player-map? @state/game e))
+  [game e]
+  (on-player-map? game e))
 
 (defn do-eyes!
   "Performed on each eye tick"
   [e]
   (go
-    (if (should-do-eyes? e)
-      (let [game @state/game
-            map (att game e :map)
-            last-points (att game e :visible-points)
-            last-visible (att game e :visible-entities)
-
-            points (into #{} (find-visible-points game e))
-            entities (into (im/int-set) (find-entities-in game map points))
-
-            now-seen-points (set/difference points last-points)]
-
-        (send state/game set-att e
-              :explored-points (into (att game e :explored-points #{}) now-seen-points)
-              :visible-points points
-              :visible-entities entities))
-      (<! (timeout 1000)))))
+    (let [game @state/game]
+      (if (should-do-eyes? game e)
+        (just-do-eyes! game e)
+        (<! (timeout 1000))))))
 
 (defn eye-loop!
   [e]
